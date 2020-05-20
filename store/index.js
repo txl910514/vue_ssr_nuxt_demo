d858c8f423df38c7edd0840728a02a5cfe9d4b76
tangxl <1092678514@qq.com>
2020-05-20 10:31:54 +0800

    提交

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ export const actions = {
     // 全局服务初始化
     nuxtServerInit(store, { req }) {
         const initFetchAppData = [
-          store.dispatch('tag/fetchList'),
+          // store.dispatch('tag/fetchList'),
           store.dispatch('tag/nextList')
         ]
         return Promise.all(initFetchAppData)
